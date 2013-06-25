1d092cac7dea57984f18c6c9535d2530d7f7f933
Blaise <blaisekal@gmail.com>
2013-06-25 22:24:52 +0200

    Setup canvas

--- script/canvas.js
+++ script/canvas.js
@@ -5,6 +5,29 @@
 Z.Canvas = function() {
     this.canvas = document.querySelector('canvas');
     this.ctx = this.canvas.getContext('2d');
+    this.width = this.canvas.getAttribute('width');
+    this.height = this.canvas.getAttribute('height');
+
+    if (!window.requestAnimationFrame) {
+        this.setVendorRequestAnimationFrame();
+    }
+
+    this.path = [];
+    this.level = [];
+    this.boats = [];
+    this.ferries = [];
+
+    this.paint();
+};
+
+Z.Canvas.prototype.paint = function() {
+    window.requestAnimationFrame(this.paint.bind(this));
+    this.ctx.clearRect(0, 0, this.width, this.height);
+
+    this.paintEntity(this.path);
+    this.paintEntity(this.level);
+    this.paintEntities(this.boats);
+    this.paintEntities(this.ferries);
 };
 
 /**
@@ -18,6 +41,27 @@ Z.Canvas.prototype.drawSubPoint = function(point) {
     this.ctx.fill();
 };
 
+Z.Canvas.prototype.paintEntities = function(entities) {
+    for (var i = 0, m = entities.length; i < m; i++) {
+        this.paintEntity(entities[i]);
+    }
+};
+
+Z.Canvas.prototype.paintEntity = function(entity) {
+
+};
+
 Z.Canvas.prototype.clear = function() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+Z.Canvas.prototype.setVendorRequestAnimationFrame = function() {
+    window.requestAnimationFrame =
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+            setTimeout(callback, 1000 / 60);
+        };
 };
