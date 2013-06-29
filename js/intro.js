63fa575cd2041e3217af0c314e6e21121107d3d2
Blaise <blaisekal@gmail.com>
2013-06-29 20:11:47 +0200

    Immediate game start (2)

--- js/intro.js
+++ js/intro.js
@@ -6,11 +6,10 @@
  * @constructor
  */
 Z.Intro = function() {
-    this.entity = new Z.Entity('img/intro.png', new Z.Point(250, 250));
+    // this.entity = new Z.Entity('img/intro.png', new Z.Point(250, 250));
     this.gameStart.bind(this);
 };
 
 Z.Intro.prototype.gameStart = function() {
     Z.game = new Z.Game();
-    document.body.onclick = null;
 };
